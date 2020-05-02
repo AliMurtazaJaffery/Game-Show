e6c0c3f32f9db757a98f233be8b28360327769df
AliMurtazaJaffery <60510673+AliMurtazaJaffery@users.noreply.github.com>
2020-05-02 22:56:38 +0500

    Update 1.cpp
    So I made 4 functions.The sample_question_structure is basically our main function. In total we will replicate this function 4 more times.I have figured everything out. Ussmai humarai different topics sai questions hongai. in total.There will be 10 calls to these functions. Between each call we will save the game and the extract game data from the file. So, abhi thorra sleepy hoon, baqi yeh 20 minute kaa kaam hai and our main thing will be completed. ab in function calls kai beech mai buss hum lucky 7 aur rapid fire ka function call karaingai. And our project will be done. So, it's ok if you want to give it a rest and wait for the quiz.

--- 1.cpp
+++ 1.cpp
@@ -61,9 +61,67 @@ void select_currency(){
 cout << "Game Currency is : " << currency << endl;
 
 }
+string ToLowerCase(string v){
+  for (int i=0; i<v.length(); i++){
+    char x=v[i];
+    if (x<='Z' && x>='A'){
+      v[i]=x-('A'-'a');
+    }
+  }
+  return v;
+}
+storing_data(int ques,int cash){
+  ofstream fout("output.txt");
+  fout<<ques<<" "<<cash;
+  fout.close();
+}
+extractng_data(int &ques,int &cash){
+  ofstream fin("output.txt");
+  fin<<ques<<cash;
+  cout<<ques<<" "<<cash;
+  fin.close();
+}
+sample_question_structure(int ques,int &index,int &cash){
+  int random;
+  string answer;
+  cout<<"Question "<<ques<<" is: "<<endl;
+  struct question_structure{
+    string question;
+    string option_A;
+    string option_B;
+    string option_C;
+    string option_D;
+    string correct_option;
+    string correct_letter;
+  };
+  question_structure game_program[5]={{"what","a","b","c","d","c"},{"why","a","b","c","d","a"}};
+  random=rand()%5;
+  if (random==index){
+    while(random==index){
+      random=rand()%5;
+    }
+  }
+  index=random;
+  cout<<left;
+  cout<<game_program[index].question<<endl;
+  cout<<"A) "<<game_program[index].option_A<<setw(25)<<"B) "<<game_program[index].option_B<<endl;
+  cout<<"C) "<<game_program[index].option_C<<setw(25)<<"D) "<<game_program[index].option_D<<endl;
+  cout<<"Your Answer: ";
+  cin<<answer;
+  if (ToLowerCase(answer)==game_program[index].correct_option || ToLowerCase(answer)==game_program[index].correct_letter){
+    cout<<"Your Answer is correct"<<endl;
+    cash=cash*2;
+  }
+  else{
+    cout<<"Incorrect Answer"<<endl;
+    cash=cash/2;
+  }
+  cout<<"Your new balance is "<<currency<<" "<<cash<<endl;
+}
 
 int main() {
     // declare all variables here with either significant names or add comments explaning their use
+    srand(time(NULL));
     int no_of_players, no_of_questions;
     string player_1;
     cout << "Welcome to Quiz 2020!" << endl;
@@ -79,10 +137,7 @@ int main() {
 
     select_currency();
 
-    fstream score;
-    score.open("score board.txt", ios::in);
-    score.open("score board.txt", ios::out);
-
+  
 
 
 
