28b8aa23e715de9fb33a8706d8194aaffa469ad7
AliMurtazaJaffery <60510673+AliMurtazaJaffery@users.noreply.github.com>
2020-05-06 19:51:35 +0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <conio.h>
+#include <cstdlib>
 #include <iomanip>
-
-/*
-//to take in number of users
-do {
-cout << endl;
-cout << "Enter Number of Players [1/2] : " ;
-cin >> no_of_players;
-if( !( no_of_players ==1 || no_of_players == 2) ) { cout << "Invalid Option, Please select again! " << endl;}
-} while( !(no_of_players == 1 || no_of_players == 2) );
-buffer();
-*/
+#include <conio.h>
 using namespace std;
-
-// delcare all global variables here, score is the game score for the players and currency is the global currency value for the entire code
-int score_1;
+//global variable
+//index will be used in the structured questions.
+int index;
 string currency;
-
 // function printd endl, and then clears it
 void buffer() {
   cout << endl;
@@ -27,16 +16,12 @@ void buffer() {
   getch();
   system("cls");
 }
-
 // function to print introduction for the game and instructions too, edit the last!!!
 void introduction (string player_1) {
-
   cout << "HEllO " << player_1 << endl;
   cout << "This is how the game works" << endl;
   buffer();
-
 }
-
 // to select game currency as score
 void select_currency(){
   int currency_options;
@@ -61,27 +46,111 @@ void select_currency(){
 cout << "Game Currency is : " << currency << endl;
 
 }
+string ToLowerCase(string v){
+  for (int i=0; i<v.length(); i++){
+    char x=v[i];
+    if (x<='Z' && x>='A'){
+      v[i]=x-('A'-'a');
+    }
+  }
+  return v;
+}
+void storing_data(int ques,int cash){
+  ofstream fout("output.txt");
+  fout<<ques<<endl;
+  fout<<cash;
+  fout.close();
+}
+void extracting_data(int &ques,int &cash){
+  ifstream fin("output.txt");
+  fin>>ques>>cash;
+  fin.close();
+}
+sample_question_structure_1(int &ques,int &cash){
+  int random;
+  string answer;
+  cout<<"Question "<<ques<<" is: "<<endl;
+  struct question_structure{
+    string question;
+    string option_A;
+    string option_B;
+    string option_C;
+    string option_D;
+    string correct_option;
+    string correct_letter;
+  };
+  question_structure game_program[5]={{"what","a","b","c","d","c","d"},{"why","a","b","c","d","a","g"},{"what","1","2","4","5","6","4"},{"when","1","3","5","5","t","5"},{"where","r","t","y","t","t","5"}};
+  random=rand()%5;
+  if (random==index){
+    while(random==index){
+      random=rand()%5;
+    }
+  }
+  index=random;
+  cout<<left;
+  cout<<game_program[index].question<<endl;
+  cout<<"A) "<<setw(25)<<game_program[index].option_A<<"B) "<<game_program[index].option_B<<endl;
+  cout<<"C) "<<setw(25)<<game_program[index].option_C<<"D) "<<game_program[index].option_D<<endl;
+  cout<<"Your Answer: ";
+  cin>>answer;
+  if (ToLowerCase(answer)==game_program[index].correct_option || ToLowerCase(answer)==game_program[index].correct_letter){
+    cout<<"Your Answer is correct"<<endl;
+    cash=cash*2;
+  }
+  else{
+    cout<<"Incorrect Answer"<<endl;
+    cash=cash/2;
+  }
+  cout<<"Your new balance is "<<currency<<" "<<cash<<endl;
+  ques++;
+  storing_data(ques,cash);
+  buffer();
+}
 
-int main() {
-    // declare all variables here with either significant names or add comments explaning their use
-    int no_of_players, no_of_questions;
+int main(){
+
+  int round,money;
+  extracting_data(round,money);
+  if (round=1){
     string player_1;
     cout << "Welcome to Quiz 2020!" << endl;
     buffer();
-
-    cout << "Enter first name of player : " ; cin >> player_1;
-    cout << endl;
+    cout << "Enter first name of player : " ;
+    cin >> player_1;
     buffer();
 
     introduction(player_1);
-
-    buffer();
-
     select_currency();
+  }
 
-    fstream score;
-    score.open("score board.txt", ios::in);
-    score.open("score board.txt", ios::out);
+
+  srand(time(NULL));
+
+  while (round<3){
+    sample_question_structure_1(round,money);
+  }
+  // while (2<round<5){
+  //   sample_question_structure_2(round,money);
+  // }
+  // if (round==5){
+  //   lucky_7(round,money);
+  // }
+  // while (5<round<8){
+  //   sample_question_structure_3(round,money);
+  // }
+  // if (round==8){
+  //   rapid_fire(round,money);
+  // }
+  // while (8<round<11){
+  //   sample_question_structure_4(round,money);
+  // }
+  // if (round==11){
+  //   lucky_7(round,money);
+  // }
+  // while (11<round<14){
+  //   sample_question_structure_5(round,money);
+  // }
+
 
 
 
